3a199c382fbc98d56887ca66136976f6332b6d80
eboy <chen881020@gmail.com>
2023-07-24 15:29:39 +0800

    string update

--- lib/core/utils/app_strings.dart
+++ lib/core/utils/app_strings.dart
@@ -2,12 +2,12 @@ abstract class AppStrings {
   static const String developerName = 'Allen Chen';
   static const String helloIM = 'Hello, I’m';
   static const String introMsg =
-      "An Entry-Level developer with 2 months of experience in coding. I have completed this portfolio website to improve my programming skill and aim to solve complex problem in the future.";
+      "An Entry-Level developer with 3 months of experience in coding. I have completed this portfolio website to improve my programming skill and aim to solve complex problem in the future.";
   static String aboutMeMsg =
-      "I'm a Flutter developer with a good understanding of the Flutter framework, I have experience in UI design, API integration, clean code writing and knowledge of solid principles and clean architecture. I have completed many projects that prove my ability to provide creative and high-quality solutions and finish them in an almost complete way with a great user experience without any problems facing users";
-  static const String numOfExperience = '2+';
+      "I'm a developer with a good understanding of Python and Flutter framework, I have build many projects about data anaylsis with Python and build my personal portfolio with Dart in Flutter framwork. I have completed some projects that prove my ability to provide creative and high-quality solutions to solve some complex problem.";
+  static const String numOfExperience = '1+';
   static const String experienceMsg =
-      'Years of experience, Specialized in building apps, while ensuring a seamless web experience for end users.';
+      'Years of experience, Specialized in data analysis.';
   static const String servicesIOffer = 'The services i offer:';
   static const String featuredProjects = 'Featured projects';
   static const String projectsMsg =
