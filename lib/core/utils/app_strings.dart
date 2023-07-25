8d4013f1565b52390f56c6173dfcd4c7effc3441
eboy <chen881020@gmail.com>
2023-07-25 13:32:47 +0800

    string update

--- lib/core/utils/app_strings.dart
+++ lib/core/utils/app_strings.dart
@@ -2,17 +2,17 @@ abstract class AppStrings {
   static const String developerName = 'Allen Chen';
   static const String helloIM = 'Hello, I’m';
   static const String introMsg =
-      "An Entry-Level developer with 3 months of experience in coding. I have completed this portfolio website to improve my programming skill and aim to solve complex problem in the future.";
+      "An entry-level developer with 3 months of coding experience. I have completed this portfolio website to enhance my programming skills and aspire to tackle complex problems in the future.";
   static String aboutMeMsg =
-      "I'm a developer with a good understanding of Python and Flutter framework, I have build many projects about data anaylsis with Python and build my personal portfolio with Dart in Flutter framwork. I have completed some projects that prove my ability to provide creative and high-quality solutions to solve some complex problem.";
+      "I'm a developer with a solid understanding of Python and the Flutter framework. I have built numerous projects focused on data analysis using Python, as well as my personal portfolio with Dart in the Flutter framework. These projects serve as evidence of my ability to deliver creative and high-quality solutions to solve complex problems";
   static const String numOfExperience = '1+';
   static const String experienceMsg =
       'Years of experience, Specialized in data analysis.';
-  static const String servicesIOffer = 'The services i offer:';
+  static const String servicesIOffer = 'The tools I use:';
   static const String featuredProjects = 'Featured projects';
   static const String projectsMsg =
-      "I have completed many projects that prove my ability to provide creative and high-quality solutions and finish them in an almost complete way with a great user experience without any problems facing users.";
+      "I have undertaken several projects that serve as evidence of my proficiency in delivering creative and high-quality solutions to solve complex problems.";
   static const String contactMsg =
-      "For any service and work you can contact with me by send an email or by our social media accounts.";
+      "If you wish to leave any messages, you may contact me by sending an email.";
   static const String contactWithMe = 'Contact with me';
 }
