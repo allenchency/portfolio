7c0b2b7657c5521e74e9100be5fe4ce1d878b19c
Tuple <chen545788@gmail.com>
2024-07-19 17:35:51 +0800

    change color

--- lib/core/utils/app_colors.dart
+++ lib/core/utils/app_colors.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
 
 abstract class AppColors {
-  static Color white = const Color(0xffffffff);
+  static Color white = Color.fromRGBO(215,220,226, 1);
   static Color lightColor = const Color(0xffF5F5F5);
   static Color lowPriority = const Color(0xff848191);
-  static Color primaryColor = const Color(0xff7562E0);
-  static Color secondaryColor = const Color(0xff282162);
+  static Color primaryColor = Color.fromRGBO(92,207,230, 1);
+  static Color secondaryColor = Color.fromRGBO(253,180,56, 1);
   static Color darkColor = const Color(0xff000036);
-  static Color scaffoldColor = const Color(0xff1A1A29);
-  static Color appBarColor = const Color(0xff181824);
+  static Color scaffoldColor = Color.fromRGBO(29,36,51, 1);
+  static Color appBarColor = Color.fromRGBO(23,28,40, 1);
   static Color primaryLight = const Color(0xff31313F);
 }
